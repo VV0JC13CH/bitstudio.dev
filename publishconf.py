3f980408bf5fdec023806b3f979ce99c4bdf8a24
Wojciech Bobrowski <wbobrowski@egnyte.com>
2020-02-11 18:54:29 +0100

    Fixing RSS link in footer

--- publishconf.py
+++ publishconf.py
@@ -14,8 +14,7 @@ from pelicanconf import *
 SITEURL = 'https://bitstudio.dev'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = SITEURL + '/feeds/all.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
